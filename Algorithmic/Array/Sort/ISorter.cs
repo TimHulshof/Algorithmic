45210265bfd91c29c1e3ffae1ee1e55135844a5c
Tim Hulshof <tim_hulshof@hotmail.com>
2021-10-16 23:08:37 -0600

    Comment ElementComparer delegate, and rework delegate invokation to make functionality more clear.

--- Algorithmic/Array/Sort/ISorter.cs
+++ Algorithmic/Array/Sort/ISorter.cs
@@ -5,12 +5,19 @@ namespace Algorithmic.Array.Sort
 {
     internal interface ISorter
     {
-        internal delegate bool LambdaComparer<T>(T previous, T current);
+        /// <summary>
+        /// Predicate to compare two elements on an array.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="lower">Element on "lower" end of array.</param>
+        /// <param name="higher">Element on "higher" end of array.</param>
+        /// <returns>True if elements are ordered lower to higher.</returns>
+        internal delegate bool ElementComparer<T>(T lower, T higher);
 
         internal IList<T> Sort<T>(IList<T> collection) where T : IComparable<T>;
         internal IList<T> Sort<T>(IList<T> collection, Comparison<T> comparison);
         internal IList<T> Sort<T>(IList<T> collection, IComparer<T>? comparer);
-        internal IList<T> Sort<T>(IList<T> collection, LambdaComparer<T> comparer);
+        internal IList<T> Sort<T>(IList<T> collection, ElementComparer<T> comparer);
 
     }
 }
--- Algorithmic/Array/Sort/SorterBase.cs
+++ Algorithmic/Array/Sort/SorterBase.cs
@@ -7,24 +7,24 @@ namespace Algorithmic.Array.Sort
     {
         IList<T> ISorter.Sort<T>(IList<T> collection)
         {
-            return SortAlgorithm(collection, (p, c) => p.CompareTo(c) > 0) ;
+            return SortAlgorithm(collection, (p, c) => p.CompareTo(c) < 0) ;
         }
 
         IList<T> ISorter.Sort<T>(IList<T> collection, Comparison<T> comparison)
         {
-            return SortAlgorithm(collection, (p, c) => comparison.Invoke(p, c) > 0);
+            return SortAlgorithm(collection, (p, c) => comparison.Invoke(p, c) < 0);
         }
 
         IList<T> ISorter.Sort<T>(IList<T> collection, IComparer<T> comparer)
         {
-            return SortAlgorithm(collection, (p, c) => comparer.Compare(p, c) > 0);
+            return SortAlgorithm(collection, (p, c) => comparer.Compare(p, c) < 0);
         }
 
-        IList<T> ISorter.Sort<T>(IList<T> collection, ISorter.LambdaComparer<T> comparer)
+        IList<T> ISorter.Sort<T>(IList<T> collection, ISorter.ElementComparer<T> comparer)
         {
             return SortAlgorithm(collection, comparer);
         }
 
-        private protected abstract IList<T> SortAlgorithm<T>(IList<T> collection, ISorter.LambdaComparer<T> comparer);
+        private protected abstract IList<T> SortAlgorithm<T>(IList<T> collection, ISorter.ElementComparer<T> comparer);
     }
 }
