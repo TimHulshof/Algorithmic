9b0efab682d0e854a9541e494d89570d23fc7cfd
Tim Hulshof <tim_hulshof@hotmail.com>
2021-10-16 22:47:47 -0600

    add array sort abstractions.

---
+++ Algorithmic/Array/ISorter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithmic.Array
+{
+    internal interface ISorter
+    {
+        internal delegate bool LambdaComparer<T>(T previous, T current);
+
+        internal IList<T> Sort<T>(IList<T> collection) where T : IComparable<T>;
+        internal IList<T> Sort<T>(IList<T> collection, Comparison<T> comparison);
+        internal IList<T> Sort<T>(IList<T> collection, IComparer<T>? comparer);
+        internal IList<T> Sort<T>(IList<T> collection, LambdaComparer<T> comparer);
+
+    }
+}
---
+++ Algorithmic/Array/SorterBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithmic.Array
+{
+    internal abstract class SorterBase : ISorter
+    {
+        IList<T> ISorter.Sort<T>(IList<T> collection)
+        {
+            return SortAlgorithm(collection, (p, c) => p.CompareTo(c) > 0) ;
+        }
+
+        IList<T> ISorter.Sort<T>(IList<T> collection, Comparison<T> comparison)
+        {
+            return SortAlgorithm(collection, (p, c) => comparison.Invoke(p, c) > 0);
+        }
+
+        IList<T> ISorter.Sort<T>(IList<T> collection, IComparer<T> comparer)
+        {
+            return SortAlgorithm(collection, (p, c) => comparer.Compare(p, c) > 0);
+        }
+
+        IList<T> ISorter.Sort<T>(IList<T> collection, ISorter.LambdaComparer<T> comparer)
+        {
+            return SortAlgorithm(collection, comparer);
+        }
+
+        private protected abstract IList<T> SortAlgorithm<T>(IList<T> collection, ISorter.LambdaComparer<T> comparer);
+    }
+}
