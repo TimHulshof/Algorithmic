4684f56e6bda67619c019a6eceebbad7af34c747
Tim Hulshof <tim_hulshof@hotmail.com>
2021-10-18 21:10:46 -0600

    Refactor tests to be more explicity and work edge cases.

--- Algorithmic.Test/Array/Sort/SorterTest.cs
+++ Algorithmic.Test/Array/Sort/SorterTest.cs
@@ -56,18 +56,13 @@ namespace Algorithmic.Array.Sort
         [DynamicData(nameof(Sorters), DynamicDataSourceType.Property)]
         public void ShouldSortValuesWhenTImplimentsIComparable(ISorter sorter)
         {
-            var mockArray = new Mock<IList<int>>();
-            for (int i = 0; i < 100; i++)
+            foreach (var testArray in GetTestArrays())
             {
-                mockArray.Setup(m => m.Add(It.IsAny<int>()));
-            }
-
-            sorter.Sort(mockArray.Object);
-
-            var sortedArray = mockArray.Object;
-            for (int i = 1; i < sortedArray.Count; i++)
-            {
-                Assert.IsTrue(sortedArray[i] > (sortedArray[i - 1]));
+                var foo = sorter.Sort(testArray);
+                for (int i = 1; i < foo.Count; i++)
+                {
+                    Assert.IsTrue(foo[i] >= (foo[i - 1]));
+                }
             }
         }
 
@@ -75,18 +70,13 @@ namespace Algorithmic.Array.Sort
         [DynamicData(nameof(Sorters), DynamicDataSourceType.Property)]
         public void ShouldSortValuesWhenIComparerTPassedAsArgument(ISorter sorter)
         {
-            var mockArray = new Mock<IList<int>>();
-            for (int i = 0; i < 100; i++)
+            foreach (var testArray in GetTestArrays())
             {
-                mockArray.Setup(m => m.Add(It.IsAny<int>()));
-            }
-
-            sorter.Sort(mockArray.Object, new ReverseIntComparer());
-
-            var sortedArray = mockArray.Object;
-            for (int i = 1; i < sortedArray.Count; i++)
-            {
-                Assert.IsTrue(sortedArray[i] < (sortedArray[i - 1]));
+                sorter.Sort(testArray, new ReverseIntComparer());
+                for (int i = 1; i < testArray.Length; i++)
+                {
+                    Assert.IsTrue(testArray[i] <= (testArray[i - 1]));
+                }
             }
         }
 
@@ -94,21 +84,37 @@ namespace Algorithmic.Array.Sort
         [DynamicData(nameof(Sorters), DynamicDataSourceType.Property)]
         public void ShouldSortValuesWhenComparisonTPassedAsArgument(ISorter sorter)
         {
-            var mockArray = new Mock<IList<int>>();
-            for (int i = 0; i < 100; i++)
+            foreach (var testArray in GetTestArrays())
             {
-                mockArray.Setup(m => m.Add(It.IsAny<int>()));
-            }
-
-            sorter.Sort(mockArray.Object, (p, c) => c - p);
-
-            var sortedArray = mockArray.Object;
-            for (int i = 1; i < sortedArray.Count; i++)
-            {
-                Assert.IsTrue(sortedArray[i] > (sortedArray[i - 1]));
+                sorter.Sort(testArray, (p, c) => c - p);
+                for (int i = 1; i < testArray.Length; i++)
+                {
+                    Assert.IsTrue(testArray[i] <= (testArray[i - 1]));
+                }
             }
         }
 
         #endregion
+
+        private IEnumerable<int[]> GetTestArrays()
+        {
+            yield return new int[] { 0, 0 };
+            yield return new int[] { 0, 0, 0 };
+            yield return new int[] { 0, 1 };
+            yield return new int[] { 1, 0 };
+            yield return new int[] { 0, 1, 2 };
+            yield return new int[] { 0, 2, 1 };
+            yield return new int[] { 1, 0, 2 };
+            yield return new int[] { 1, 2, 0 };
+            yield return new int[] { 2, 1, 0 };
+            yield return new int[] { 2, 0, 1 };
+            yield return new int[] { 0, 1, 1 };
+            yield return new int[] { 1, 0, 1 };
+            yield return new int[] { 1, 1, 0 };
+            yield return new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            yield return new int[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+            yield return new int[] { -1, -2, -3, -4, -5, -6, -7, -8, -9, -10 };
+            yield return new int[] { 12, 6, 3, 13, 65, 1, 78, 1, 31, 21, -1, -5, 213, -7134, 134 };
+        }
     }
 }
