30e9b2ce11d72f687686e6fef729154b87c9b780
Tim Hulshof <tim_hulshof@hotmail.com>
2021-10-18 20:42:32 -0600

    Test sorting output is in correct order.

---
+++ Algorithmic.Test/Array/Sort/ReverseIntComparer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithmic.Array.Sort
+{
+    internal class ReverseIntComparer : IComparer<int>
+    {
+        public int Compare(int x, int y)
+        {
+            if (x == y)
+                return 0;
+            if (x < y)
+                return 1;
+            if (x > y)
+                return -1;
+            else
+                throw new Exception();
+        }
+    }
+}
--- Algorithmic.Test/Array/Sort/SorterTest.cs
+++ Algorithmic.Test/Array/Sort/SorterTest.cs
@@ -49,5 +49,66 @@ namespace Algorithmic.Array.Sort
                 Assert.AreEqual(mockArray.Object[i], mockClone.Object[i]);
             }
         }
+
+        #region General sorting algorithm tests.
+
+        [DataTestMethod]
+        [DynamicData(nameof(Sorters), DynamicDataSourceType.Property)]
+        public void ShouldSortValuesWhenTImplimentsIComparable(ISorter sorter)
+        {
+            var mockArray = new Mock<IList<int>>();
+            for (int i = 0; i < 100; i++)
+            {
+                mockArray.Setup(m => m.Add(It.IsAny<int>()));
+            }
+
+            sorter.Sort(mockArray.Object);
+
+            var sortedArray = mockArray.Object;
+            for (int i = 1; i < sortedArray.Count; i++)
+            {
+                Assert.IsTrue(sortedArray[i] > (sortedArray[i - 1]));
+            }
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(Sorters), DynamicDataSourceType.Property)]
+        public void ShouldSortValuesWhenIComparerTPassedAsArgument(ISorter sorter)
+        {
+            var mockArray = new Mock<IList<int>>();
+            for (int i = 0; i < 100; i++)
+            {
+                mockArray.Setup(m => m.Add(It.IsAny<int>()));
+            }
+
+            sorter.Sort(mockArray.Object, new ReverseIntComparer());
+
+            var sortedArray = mockArray.Object;
+            for (int i = 1; i < sortedArray.Count; i++)
+            {
+                Assert.IsTrue(sortedArray[i] < (sortedArray[i - 1]));
+            }
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(Sorters), DynamicDataSourceType.Property)]
+        public void ShouldSortValuesWhenComparisonTPassedAsArgument(ISorter sorter)
+        {
+            var mockArray = new Mock<IList<int>>();
+            for (int i = 0; i < 100; i++)
+            {
+                mockArray.Setup(m => m.Add(It.IsAny<int>()));
+            }
+
+            sorter.Sort(mockArray.Object, (p, c) => c - p);
+
+            var sortedArray = mockArray.Object;
+            for (int i = 1; i < sortedArray.Count; i++)
+            {
+                Assert.IsTrue(sortedArray[i] > (sortedArray[i - 1]));
+            }
+        }
+
+        #endregion
     }
 }
