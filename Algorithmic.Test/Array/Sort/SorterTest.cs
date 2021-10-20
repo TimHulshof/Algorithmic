790eef6d05b2ad3651d31a831cbf868a6dafa819
Tim Hulshof <tim_hulshof@hotmail.com>
2021-10-19 23:05:53 -0600

    Refactor sorting test to output failed sort values.

--- Algorithmic.Test/Array/Sort/SorterTest.cs
+++ Algorithmic.Test/Array/Sort/SorterTest.cs
@@ -2,6 +2,7 @@
 using Moq;
 using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace Algorithmic.Array.Sort
 {
@@ -64,7 +65,7 @@ namespace Algorithmic.Array.Sort
                 sorter.Sort(testArrays[i]);
                 for (int j = 1; j < testArrays[i].Length; j++)
                 {
-                    Assert.IsTrue(testArrays[i][j] >= (testArrays[i][j - 1]), $"Failed on testArray[{i}]");
+                    Assert.IsTrue(testArrays[i][j] >= (testArrays[i][j - 1]), $"Failed on testArray[{i}]\nSorted output: {GetIntArrayAsString(testArrays[i])}");
                 }
             }
         }
@@ -79,7 +80,7 @@ namespace Algorithmic.Array.Sort
                 sorter.Sort(testArrays[i], new ReverseIntComparer());
                 for (int j = 1; j < testArrays[i].Length; j++)
                 {
-                    Assert.IsTrue(testArrays[i][j] <= (testArrays[i][j - 1]), $"Failed on testArray[{i}]");
+                    Assert.IsTrue(testArrays[i][j] <= (testArrays[i][j - 1]), $"Failed on testArray[{i}]\nSorted output: {GetIntArrayAsString(testArrays[i])}");
                 }
             }
         }
@@ -110,7 +111,7 @@ namespace Algorithmic.Array.Sort
                 sorter.Sort(testArrays[i], comparison);
                 for (int j = 1; j < testArrays[i].Length; j++)
                 {
-                    Assert.IsTrue(testArrays[i][j] <= (testArrays[i][j - 1]), $"Failed on testArray[{i}]");
+                    Assert.IsTrue(testArrays[i][j] <= (testArrays[i][j - 1]), $"Failed on testArray[{i}]\nSorted output: {GetIntArrayAsString(testArrays[i])}");
                 }
             }
         }
@@ -119,35 +120,35 @@ namespace Algorithmic.Array.Sort
 
         private List<int[]> GetTestArrays()
         {
-            return new List<int[]>()
+            return new List<int[]>()                                                        // Array index.
             {
-                new int[] { 0, 0 },
-                new int[] { 0, 0, 0 },
-                new int[] { 0, 1 },
-                new int[] { 1, 0 },
-                new int[] { 0, 1, 2 },
-                new int[] { 0, 2, 1 },
-                new int[] { 1, 0, 2 },
-                new int[] { 1, 2, 0 },
-                new int[] { 2, 1, 0 },
-                new int[] { 2, 0, 1 },
-                new int[] { 0, 1, 1 },
-                new int[] { 1, 0, 1 },
-                new int[] { 1, 1, 0 },
-                new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 },
-                new int[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 },
-                new int[] { -1, -2, -3, -4, -5, -6, -7, -8, -9, -10 },
-                new int[] { 12, 6, 3, 13, 65, 1, 78, 1, 31, 21, -1, -5, 213, -7134, 134 },
-                GetRandomIntArray(100, int.MaxValue),
-                GetRandomIntArray(100, int.MaxValue - 1),
-                GetRandomIntArray(100, int.MinValue),
-                GetRandomIntArray(100, int.MinValue + 1),
-                GetRandomIntArray(100),
-                GetRandomIntArray(100),
-                GetRandomIntArray(100),
-                GetRandomIntArray(100),
-                GetRandomIntArray(100),
-                GetRandomIntArray(100),
+                new int[] { 0, 0 },                                                         // 0
+                new int[] { 0, 0, 0 },                                                      // 1
+                new int[] { 0, 1 },                                                         // 2
+                new int[] { 1, 0 },                                                         // 3
+                new int[] { 0, 1, 2 },                                                      // 4
+                new int[] { 0, 2, 1 },                                                      // 5
+                new int[] { 1, 0, 2 },                                                      // 6
+                new int[] { 1, 2, 0 },                                                      // 7
+                new int[] { 2, 1, 0 },                                                      // 8
+                new int[] { 2, 0, 1 },                                                      // 9
+                new int[] { 0, 1, 1 },                                                      // 10
+                new int[] { 1, 0, 1 },                                                      // 11
+                new int[] { 1, 1, 0 },                                                      // 12
+                new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 },                                // 13
+                new int[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 },                                // 14
+                new int[] { -1, -2, -3, -4, -5, -6, -7, -8, -9, -10 },                      // 15
+                new int[] { 12, 6, 3, 13, 65, 1, 78, 1, 31, 21, -1, -5, 213, -7134, 134 },  // 16
+                GetRandomIntArray(100, int.MaxValue),                                       // 17
+                GetRandomIntArray(100, int.MaxValue - 1),                                   // 18
+                GetRandomIntArray(100, int.MinValue),                                       // 19
+                GetRandomIntArray(100, int.MinValue + 1),                                   // 20
+                GetRandomIntArray(100),                                                     // 21
+                GetRandomIntArray(100),                                                     // 22
+                GetRandomIntArray(100),                                                     // 23
+                GetRandomIntArray(100),                                                     // 24
+                GetRandomIntArray(100),                                                     // 25
+                GetRandomIntArray(100),                                                     // 26
             };
         }
 
@@ -166,6 +167,16 @@ namespace Algorithmic.Array.Sort
                 array[index] = (int)injectedValue;
             }
             return array;
+        }
+
+        private string GetIntArrayAsString(int[] array)
+        {
+            var stringBuilder = new StringBuilder();
+            foreach(var element in array)
+            {
+                stringBuilder.Append(element.ToString() + " ");
+            }
+            return stringBuilder.ToString();
         }
     }
 }
