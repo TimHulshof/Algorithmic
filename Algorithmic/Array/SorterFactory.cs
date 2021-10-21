43b1498e62fcac618b1598da08b6de7723f4e92c
Tim Hulshof <tim_hulshof@hotmail.com>
2021-10-20 21:07:05 -0600

    Impliment sorter factory.

---
+++ Algorithmic/Array/SorterFactory.cs
@@ -0,0 +1,26 @@
+﻿using Algorithmic.Array.Sort;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Algorithmic.Array
+{
+    public class SorterFactory
+    {
+        private ISorter[] _sorters;
+
+        public SorterFactory()
+        {
+            var sorterType = typeof(ISorter);
+            _sorters = AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(a => a.GetTypes())
+                .Where(t => sorterType.IsAssignableFrom(t) && !t.IsInterface && !t.IsAbstract)
+                .Select(s => Activator.CreateInstance(s) as ISorter).ToArray();
+        }
+
+        public IEnumerable<ISorter> GetAllSorters()
+        {
+            return _sorters;       
+        }
+    }
+}
